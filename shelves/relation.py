56640389fb4f5d4140502f582b69428758c96db8
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2019-01-26 13:14:46 +0300

    Some comments

--- shelves/relation.py
+++ shelves/relation.py
@@ -1,6 +1,8 @@
 
 class Relation:
     # Item and catalog relations
+    #    first item is group/kit
     REL_INCLUDES = 1
     # Main item in the kit
+    #   first item is kit, second is main
     REL_MAIN_ITEM = 2
