7d870ecaa315be0cbdadccf07c4ab44d6fd9b81d
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2019-01-28 12:39:38 +0300

    Created JSX class for catalog item logo

--- shelves/static/catalog.js
+++ shelves/static/catalog.js
@@ -1,5 +1,21 @@
 /* Catalog item logo */
-function Logo(props) {
-    //return { props.id };
-    return <h1>Hello!</h1>;
+class Logo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {id:props.id, img_id: -1};
+    }
+
+    componentDidMount() {
+        fetch(flask_util.url_for('catalog._get_logo', { id:this.state.id }))
+           .then(response => response.json())
+           .then(data => { if (data.result) this.setState({img_id:data.result.id}); });
+    }
+
+    render() {
+        if (this.state.img_id == -1) {
+            return <span class="text-muted"><i class="fas fa-laptop fa-4x"></i></span>;
+        } else {
+            return <img src={ flask_util.url_for('uploads.view', { id:this.state.img_id } ) }/>;
+        }
+    }
 }
