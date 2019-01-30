72857a5c48b80ec2b18996e1eb01a464f3993fad
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2019-01-30 17:24:20 +0300

    Improved the catalog item list

--- shelves/static/catalog.jsx
+++ shelves/static/catalog.jsx
@@ -2,11 +2,11 @@
 class Logo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {id:props.id, img_id: -1};
+        this.state = {img_id: -1};
     }
 
     componentDidMount() {
-        fetch(flask_util.url_for('catalog._get_logo', { id:this.state.id }))
+        fetch(flask_util.url_for('catalog._get_logo', { id:this.props.id }))
            .then(response => response.json())
            .then(data => { if (data) this.setState({img_id:data.id}); });
     }
@@ -23,34 +23,34 @@ class Logo extends React.Component {
 class CatalogItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {item:props.item};
     }
 
     render() {
         return <div class="col-4">
                  <div class="row pt-4">
                    <div class="col-3 align-self-center">
-                     <Logo id={this.state.item.id} />
+                     <Logo id={this.props.item.id} />
                    </div>
                    <div class="col-9">
                      <a class="action" href={flask_util.url_for('catalog.view',
-                                             {id:this.state.item.id})}>
-                       <h5>{this.state.item.type_title ? this.state.item.type_title + " : " : ""}
-                           {this.state.item.title_eng ? this.state.item.title_eng : this.state.item.title}</h5>
+                                             {id:this.props.item.id})}>
+                       <h5>{this.props.item.type_title && !this.props.notype
+                                ? this.props.item.type_title + " : " : ""}
+                           {this.props.item.title_eng ? this.props.item.title_eng : this.props.item.title}</h5>
                      </a>
-                 <h6 class="text-secondary">{this.state.item.title_eng ? this.state.item.title : ""}</h6>
+                 <h6 class="text-secondary">{this.props.item.title_eng ? this.props.item.title : ""}</h6>
                  <p>
-                    <span class="badge badge-secondary">{this.state.item.year}</span>
+                    <span class="badge badge-secondary">{this.props.item.year}</span>
                     &nbsp;
-                    {this.state.item.company
+                    {this.props.item.company
                         && <a class="text-secondary"
-                            href={flask_util.url_for('company.view', {id:this.state.item.company_id})}>
-                            {this.state.item.company}
+                            href={flask_util.url_for('company.view', {id:this.props.item.company_id})}>
+                            {this.props.item.company}
                         </a>}
                     &nbsp;
-                    {this.state.item.count > 0
+                    {this.props.item.count > 0
                         && <span class="badge badge-secondary">{
-                            this.state.item.count + ' item' + (this.state.item.count > 1 ? 's' : '')
+                            this.props.item.count + ' item' + (this.props.item.count > 1 ? 's' : '')
                         }</span>}
                  </p>
                  </div></div>
@@ -60,26 +60,30 @@ class CatalogItem extends React.Component {
 
 function CatalogListRow(props) {
     return <div class="row pt-4">
-             { props.row.map((item) => <CatalogItem item={item} />)}
+             { props.row.map((item) => <CatalogItem item={item} notype={props.notype}/>)}
            </div>;
 }
 
 class CatalogList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {parent:props.parent, rows:null};
+        this.state = {rows:null};
     }
 
     componentDidMount() {
-        fetch(flask_util.url_for('catalog._filtered_list', { parent:this.state.parent }))
+        // TODO: expand the filter manually
+        fetch(flask_util.url_for('catalog._filtered_list', { ...this.props }))
            .then(response => response.json())
            .then(data => {
                 // split data into triples
                 var rows = [];
                 while (data.length) {
-                    rows.push(data.splice(0, 30));
+                    rows.push(data.splice(0, 3));
                 }
                 this.setState({rows:rows});
+                if (rows.length > 0 && this.props.onLoadItems) {
+                    this.props.onLoadItems();
+                }
             });
     }
 
@@ -87,7 +91,31 @@ class CatalogList extends React.Component {
         if (!this.state.rows)
             return <div></div>;
         return <div>
-                 {this.state.rows.map((row) => <CatalogListRow row={row}/>)}
+                 {this.state.rows.map((row) =>
+                    <CatalogListRow row={row} notype={this.props.notype}/>)}
+               </div>;
+    }
+}
+
+class CatalogListSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {empty:true};
+        this.onLoadItems = this.onLoadItems.bind(this);
+    }
+
+    onLoadItems() {
+        this.setState({empty:false});
+    }
+
+    render() {
+        return <div>
+                 {!this.state.empty &&
+                   <div class="row"><div class="col-12">
+                     <h3 class="pt-4">{this.props.title}</h3>
+                   </div></div>
+                 }
+                 <CatalogList {...this.props} onLoadItems={this.onLoadItems}/>
                </div>;
     }
 }
