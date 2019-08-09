a32650a4ca72d63ae109656b5f9bcac55dad59ec
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2019-08-09 16:54:25 +0300

    Added catalog pagination

--- react_shelves/src/Catalog/ListSection.js
+++ react_shelves/src/Catalog/ListSection.js
@@ -2,9 +2,13 @@ import React, { Component, Fragment } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import Pagination from 'react-bootstrap/Pagination'
 import fetchBackend from '../Backend';
 import FormCatalogCreate from '../Forms/CatalogCreate';
 import { CatalogListRow } from './Helpers';
+
+const PageCount = 300;
 
 export class CatalogListSection extends Component {
     constructor(props) {
@@ -13,6 +17,8 @@ export class CatalogListSection extends Component {
             loading: true,
             rows: [],
             showFormCreate: false,
+            page: 0,
+            count: -1,
         };
     }
     handleUpdate = () => {
@@ -20,7 +26,14 @@ export class CatalogListSection extends Component {
             this.setState({loading: false});
             return;
         }
-        fetchBackend('catalog/_filtered_list', this.props.filter)
+        fetchBackend('catalog/_filtered_count', this.props.filter)
+            .then(response => response.json())
+            .then(data => this.setState({count: data.count}))
+            .catch(e => {});
+        fetchBackend('catalog/_filtered_list',
+            {...this.props.filter,
+                limitFirst: this.state.page * PageCount,
+                limitPage: PageCount})
             .then(response => response.json())
             .then(data => {
                 var rows = [];
@@ -40,6 +53,9 @@ export class CatalogListSection extends Component {
     handleFormCreateClose = event => {
         this.setState({ showFormCreate: false });
     };
+    handlePage = page => {
+        this.setState({ page: page }, this.handleUpdate());
+    }
     render() {
         if (this.state.loading) {
             return (<div className="row"><div className="col-12">
@@ -48,13 +64,22 @@ export class CatalogListSection extends Component {
                 </h3>
             </div></div>);
         }
+        const pages = Math.ceil(this.state.count / PageCount);
+        const page = this.state.page;
+        var ellipsis1 = false;
+        var ellipsis2 = false;
         return <Fragment>
-            {(this.state.rows.length > 0 || this.props.addButton)
+            {(this.state.count > 0 || this.props.addButton)
                 && <Row>
                     <Col>
                         <h3 className="pt-4">
                             {this.props.title} &nbsp;
-                       {(this.props.addButton
+                            {this.state.count > 0
+                              ? <Badge variant="secondary">
+                                  {this.state.count}
+                                  {" item"}{this.state.count > 1 ? "s" : ""}</Badge>
+                              : <div/>} &nbsp;
+                            {(this.props.addButton
                                 && this.props.auth
                                 && this.props.auth.isAuthenticated)
                                 ? <Button type="button" className="btn btn-primary" onClick={this.handleCreateButton}>
@@ -65,6 +90,37 @@ export class CatalogListSection extends Component {
                     </Col>
                 </Row>}
             {this.state.rows.map((row) => <CatalogListRow key={row[0].id /*TODO*/} row={row} notype={this.props.filter.notype} />)}
+            {this.state.count > 0
+              ? <Row className="pt-4">
+                <Col>
+                    <Pagination>
+                        <Pagination.Prev disabled={page === 0}
+                            onClick={() => this.handlePage(page - 1)} />
+                        {[...Array(pages).keys()].map(p => {
+                            if ((p >= page - 1 && p <= page + 1) || p === 0 || p === pages - 1)
+                                return <Pagination.Item key={p}
+                                    onClick={() => this.handlePage(p)}
+                                    active={page === p}>{p + 1}
+                                </Pagination.Item>;
+                            if (p < page - 1 && !ellipsis1)
+                            {
+                                ellipsis1 = true;
+                                return <Pagination.Ellipsis disabled key={p}/>;
+                            }
+                            if (p > page + 1 && !ellipsis2)
+                            {
+                                ellipsis2 = true;
+                                return <Pagination.Ellipsis disabled key={p}/>;
+                            }
+                            return "";
+                        })}
+                        <Pagination.Next disabled={page === pages - 1}
+                            onClick={() => this.handlePage(page + 1)} />
+                    </Pagination>
+                </Col>
+                </Row>
+              : <div/>
+            }
             {(this.props.addButton && this.props.auth
                 && this.props.auth.isAuthenticated)
                 ? <FormCatalogCreate open={this.state.showFormCreate} onClose={this.handleFormCreateClose} handleUpdateItems={this.handleUpdate} {...this.props.filter} />
